a7999d5f3ecfb194139c30978d49a6e4327329d3
quyet0nguyen <43262313+quyet0nguyen@users.noreply.github.com>
2021-01-26 10:48:24 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,20 +4,19 @@ import residual_model
 from argparse import ArgumentParser
 from torch.autograd import Variable
 import PSNR
+import model
 
 def get_args():
     parser = ArgumentParser(description='generate seismic image using cycleGAN')
     parser.add_argument('--batch_size', type=int, default = 64)
     parser.add_argument('--image_size', type=int, default = 32)
     parser.add_argument('--dir', type=str, default='../AiCrowdData/data_train/data.npy')
-    #parser.add_argument('--num_iter_test', type=int, default=10)
     parser.add_argument('--G12_dir', type=str, default='./generator_state_seismic_to_noise_0.pt')
     parser.add_argument('--G21_dir', type=str, default='./generator_state_noise_to_seismic_0.pt')
     args = parser.parse_args()
     return args
 
-def get_model(dir):
-    model = residual_model.Generator(1,1)
+def get_model(model ,dir):
     model = model.float()
 
     if torch.cuda.is_available():
@@ -39,8 +38,10 @@ def main():
         test = test.cuda()
         noised = noised.cuda()
 
-    G_12 = get_model(args.G12_dir)
-    G_21 = get_model(args.G21_dir)
+    G_12 = get_model(model.G12(args.batch_size), args.G12_dir)
+    G_21 = get_model(model.G21(args.batch_size), args.G21_dir)
+    # G_12 = get_model(args.G12_dir)
+    # G_21 = get_model(args.G21_dir)
 
     G_12.eval()
     G_21.eval()
@@ -48,7 +49,7 @@ def main():
     generate_seismic_image = G_21(test.float())
     generate_noise_image = G_12(noised.float())
 
-    loss = PSNR()
+    loss = PSNR.PSNR()
 
     estimate_loss_generate_seismic = loss(generate_seismic_image, test)
     estimate_loss_noise_seismic = loss(generate_noise_image, noised)
